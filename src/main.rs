05f2d886fb5cfd5daa9daaa29561e26b45a9cb05
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-12-05 13:13:49 -0800

    Perform request for word passed on the cli

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate curl;
-use curl::http;
-
 mod rhyme;
+
+use curl::http;
 use rhyme::*;
+use std::env;
 
 fn main() {
-    let resp = http::handle()
-        .get("http://rhymebrain.com/talk?function=getRhymes&word=heart")
-        .exec().unwrap();
+    let word = env::args().nth(1).unwrap_or("heart".to_string());
+    let url = format!("http://rhymebrain.com/talk?function=getRhymes&word={}", word);
+    let resp = http::handle().get(url).exec().unwrap();
+
+    println!("{}", resp);
 }
